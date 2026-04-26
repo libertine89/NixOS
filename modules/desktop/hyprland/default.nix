bad6efcd20bc4d8d14210c25e3cdb1cbe3e6a542
libertine89 <38113173+libertine89@users.noreply.github.com>
2026-04-26 14:40:10 +0100

    Update default.nix

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -181,7 +181,7 @@ in
               "col.active_border" = "rgba(ca9ee6ff) rgba(f2d5cfff) 45deg";
               "col.inactive_border" = "rgba(b4befecc) rgba(6c7086cc) 45deg";
               resize_on_border = true;
-              layout = "dwindle"; # dwindle, master, scrolling, monocle
+              layout = "scrolling"; # dwindle, master, scrolling, monocle
               # allow_tearing = true; # Allow tearing for games (use immediate window rules for specific games or all titles)
             };
             decoration = {
