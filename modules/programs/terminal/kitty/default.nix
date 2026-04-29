49d476ad3e6bb6022bdccf8c5505fa9e5f6886b1
libertine89 <38113173+libertine89@users.noreply.github.com>
2026-04-29 21:19:17 +0100

    Update default.nix cursor trail

--- modules/programs/terminal/kitty/default.nix
+++ modules/programs/terminal/kitty/default.nix
@@ -11,9 +11,9 @@
         themeFile = "Catppuccin-Mocha";
         settings = {
           # shell = "${getExe pkgs.tmux}";
-          # cursor_trail = 3; # Fancy cursor movements (especially in nixvim)
-          # cursor_trail_decay = "0.08 0.3"; # Animation speed
-          # cursor_trail_start_threshold = "4";
+          cursor_trail = 3; # Fancy cursor movements (especially in nixvim)
+          cursor_trail_decay = "0.08 0.3"; # Animation speed
+          cursor_trail_start_threshold = "4";
           strip_trailing_spaces = "smart";
           macos_option_as_alt = "yes";
           macos_quit_when_last_window_closed = true;
