e88a4a87027bee08ca84c91b9917b91f19736718
libertine89 <38113173+libertine89@users.noreply.github.com>
2026-04-30 07:32:25 +0100

    Update default.nix

--- modules/desktop/hyprland/confs/default.nix
+++ modules/desktop/hyprland/confs/default.nix
@@ -10,5 +10,5 @@
   windowrules = import ./windowrules.nix { inherit ctx; };
   windows = import ./windows.nix { inherit ctx; };
   workspaces = import ./workspaces.nix { inherit ctx; };
-  layouts = import ./layouts.nix { inherit ctx; };
+  layers = import ./layers.nix { inherit ctx; };
 }
