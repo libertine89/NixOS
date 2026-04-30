b1371173404783a012ab1847984775702e08bdc7
libertine89 <38113173+libertine89@users.noreply.github.com>
2026-04-30 07:27:33 +0100

    Update autostart.nix

--- modules/desktop/hyprland/confs/autostart.nix
+++ modules/desktop/hyprland/confs/autostart.nix
@@ -1,4 +1,27 @@
-{ lib, pkgs, getExe, getExe', browser, terminal, fileManager, ... }:
-{
+{ ctx }:
 
+let
+  inherit (ctx)
+    pkgs lib getExe getExe'
+    browser terminal fileManager bar batterynotify;
+in
+{
+  exec-once = [
+    #"[workspace 1 silent] ${terminal}"
+    #"[workspace 5 silent] ${browser}"
+    #"[workspace 6 silent] spotify"
+    #"[workspace special silent] ${browser} --private-window"
+    #"[workspace special silent] ${terminal}"
+
+    "${lib.getExe wallpaper}"
+    "${bar}"
+    "swaync"
+    "nm-applet --indicator"
+    # "wl-clipboard-history -t"
+    "${getExe' pkgs.wl-clipboard "wl-paste"} --type text --watch cliphist store" # clipboard store text data
+    "${getExe' pkgs.wl-clipboard "wl-paste"} --type image --watch cliphist store" # clipboard store image data
+    "rm '$XDG_CACHE_HOME/cliphist/db'" # Clear clipboard
+    "${getExe batterynotify}" # battery notification
+    "polkit-agent-helper-1"
+  ];
 }
