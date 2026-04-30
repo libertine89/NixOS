480e55ea2dd1d671f3aefaf45efe6bb893957194
libertine89 <38113173+libertine89@users.noreply.github.com>
2026-04-30 07:23:02 +0100

    Update decorations.nix

--- modules/desktop/hyprland/confs/decorations.nix
+++ modules/desktop/hyprland/confs/decorations.nix
@@ -1,4 +1,22 @@
-{ lib, pkgs, getExe, getExe', browser, terminal, fileManager, ... }:
-{
+{ ctx }:
 
+let
+  inherit (ctx)
+    pkgs lib getExe getExe';
+in
+{
+  decoration = {
+    shadow.enabled = false;
+    rounding = 10;
+    dim_special = 0.3;
+    blur = {
+      enabled = true;
+      special = true;
+      size = 6; # 6
+      passes = 2; # 3
+      new_optimizations = true;
+      ignore_opacity = true;
+      xray = false;
+    };
+  };
 }
