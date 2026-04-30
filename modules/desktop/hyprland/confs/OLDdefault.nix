5a3812988ce550b35cdaa3cbebf2f2a597d0c8ba
libertine89 <38113173+libertine89@users.noreply.github.com>
2026-04-30 16:15:37 +0100

    Update and rename default.nix to OLDdefault.nix

--- modules/desktop/hyprland/confs/OLDdefault.nix
+++ modules/desktop/hyprland/confs/OLDdefault.nix
@@ -1,6 +1,5 @@
 { ctx }:
 
-let
 {
   env = import ./environments.nix { inherit ctx; };
   autostart = import ./autostart.nix { inherit ctx; };
@@ -12,24 +11,4 @@ let
   general = import ./general.nix { inherit ctx; };
   workspaces = import ./workspaces.nix { inherit ctx; };
   layerrules = import ./layerrules.nix { inherit ctx; };
-}
-in
-{
- settings = {
-    # base/global config still here
-    "$mainMod" = "SUPER";
-    "$term" = "${getExe pkgs.${terminal}}";
-    "$editor" = "code --disable-gpu";
-    "$browser" = browser;
-  }
-  // conf.env
-  // conf.autostart 
-  // conf.animations
-  // conf.decorations
-  // conf.keybindings
-  // conf.monitors
-  // conf.workspaces
-  // conf.windowrules
-  // conf.general
-  // conf.layerrules;
 }
