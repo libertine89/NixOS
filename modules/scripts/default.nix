931db7d8af00703fce982f0a9360042d9b07a6f1
libertine89 <38113173+libertine89@users.noreply.github.com>
2026-04-30 13:02:46 +0100

    Update default.nix

--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -22,6 +22,7 @@ let
 
   scripts = [
     (import ./rebuild.nix scriptArgs)
+    (import ./update.nix scriptArgs)
     (import ./rollback.nix scriptArgs)
     (import ./launcher.nix scriptArgs)
     (import ./network.nix scriptArgs)
