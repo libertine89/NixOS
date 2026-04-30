d7f6b3eebc0b69d09f19d7a6f0a6c563d52ab9b1
libertine89 <38113173+libertine89@users.noreply.github.com>
2026-04-30 13:32:31 +0100

    Update keybindings.nix

--- modules/desktop/hyprland/confs/keybindings.nix
+++ modules/desktop/hyprland/confs/keybindings.nix
@@ -123,10 +123,10 @@ in
     "$mainMod SHIFT, J, movewindow, d"
 
     # Move window to workspaces relative to the active workspace with mainMod + CTRL + SHIFT + [←→]
-    "$mainMod CTRL, SHIFT, right, exec, ${getExe wrap-window-workspace-next}"
-    "$mainMod CTRL, SHIFT, left, exec, ${getExe wrap-window-workspace-prev}"
-    "$mainMod CTRL, SHIFT, L, exec, ${getExe wrap-window-workspace-next}"
-    "$mainMod CTRL, SHIFT, H, exec, ${getExe wrap-window-workspace-prev}"
+    "$mainMod CTRL SHIFT, right, exec, ${getExe wrap-window-workspace-next}"
+    "$mainMod CTRL SHIFT, left, exec, ${getExe wrap-window-workspace-prev}"
+    "$mainMod CTRL SHIFT, L, exec, ${getExe wrap-window-workspace-next}"
+    "$mainMod CTRL SHIFT, H, exec, ${getExe wrap-window-workspace-prev}"
 
     # Switch scrolling columns
     "$mainMod, period, layoutmsg, move +col"
