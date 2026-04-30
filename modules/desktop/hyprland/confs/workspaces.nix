fcc75d813cd85e2ae6df988dfde49514b5cc1c4c
libertine89 <38113173+libertine89@users.noreply.github.com>
2026-04-30 07:17:09 +0100

    Update workspaces.nix

--- modules/desktop/hyprland/confs/workspaces.nix
+++ modules/desktop/hyprland/confs/workspaces.nix
@@ -1,4 +1,20 @@
-{ lib, pkgs, getExe, getExe', browser, terminal, fileManager, ... }:
-{
+{ ctx }:
 
+let
+  inherit (ctx)
+    pkgs lib getExe getExe'
+    browser terminal fileManager bar
+    keybinds-yad toggle-layout wrap-workspaces-next
+    wrap-workspaces-prev zoom gamemode clipmanager
+    screen-record screenshot keyboardswitch fileManagerScript;
+in
+{
+  workspace = [
+    # Binds workspaces to my monitors (find desc with: hyprctl monitors)
+    "1, persistent:true,default:true"
+    "2, persistent:true"
+    "3, persistent:true"
+    "4, persistent:true"
+    "5, persistent:true"
+  ];
 }
