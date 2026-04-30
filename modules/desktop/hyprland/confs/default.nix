a1eab3774754c521ea250abe8e31a7ddb21f67c7
libertine89 <38113173+libertine89@users.noreply.github.com>
2026-04-30 11:33:17 +0100

    Update default.nix

--- modules/desktop/hyprland/confs/default.nix
+++ modules/desktop/hyprland/confs/default.nix
@@ -4,7 +4,7 @@
   env = import ./enviroments.nix { inherit ctx; };
   autostart = import ./autostart.nix { inherit ctx; };
   animations = import ./animations.nix { inherit ctx; };
-  decoration = import ./decorations.nix { inherit ctx; };
+  decorations = import ./decorations.nix { inherit ctx; };
   keybindings = import ./keybindings.nix { inherit ctx; };
   monitors = import ./monitors.nix { inherit ctx; };
   windowrules = import ./windowrules.nix { inherit ctx; };
