901d2ab96d048873b6e01fbf943ea75e914a870b
libertine89 <38113173+libertine89@users.noreply.github.com>
2026-05-01 19:35:41 +0100

    Refactor starship configuration structure

--- modules/core/starship.nix
+++ modules/core/starship.nix
@@ -1,95 +1,102 @@
-programs.starship = {
-  enable = true;
-  settings = {
-    format = "[░▒▓](#0A0373)[ 󱄅 ](bg:#0A0373 fg:#E6E6FF)[](fg:#0A0373 bg:#1C1690)$directory[](fg:#1C1690 bg:#3A36B5)$fill[](fg:#2A2680 bg:#2A2680)$git_branch$git_status[](fg:#1C1970 bg:#1C1970)$nodejs$bun$rust$golang$php[](fg:#020152 bg:#020152)$time[▓▒░](fg:#020152)\n$character";
+{ ... }:
+{
+  home-manager.sharedModules = [
+    (_: {
+      programs.starship = {
+        enable = true;
+        settings = {
+          format = "[░▒▓](#0A0373)[ 󱄅 ](bg:#0A0373 fg:#E6E6FF)[](fg:#0A0373 bg:#1C1690)$directory[](fg:#1C1690 bg:#3A36B5)$fill[](fg:#2A2680 bg:#2A2680)$git_branch$git_status[](fg:#1C1970 bg:#1C1970)$nodejs$bun$rust$golang$php[](fg:#020152 bg:#020152)$time[▓▒░](fg:#020152)\n$character";
 
-    fill = {
-      symbol = " ";
-    };
+          fill = {
+            symbol = " ";
+          };
 
-    directory = {
-      style = "fg:#E6E6FF bg:#1C1690";
-      format = "[ $path ]($style)";
-      truncation_length = 3;
-      truncation_symbol = "…/";
-    };
+          directory = {
+            style = "fg:#E6E6FF bg:#1C1690";
+            format = "[ $path ]($style)";
+            truncation_length = 3;
+            truncation_symbol = "…/";
+          };
 
-    directory.substitutions = {
-      "Documents" = "󰈙 ";
-      "Downloads" = " ";
-      "Music" = " ";
-      "Pictures" = " ";
-    };
+          directory.substitutions = {
+            "Documents" = "󰈙 ";
+            "Downloads" = " ";
+            "Music" = " ";
+            "Pictures" = " ";
+          };
 
-    git_branch = {
-      symbol = "";
-      style = "bg:#2A2680";
-      format = "[[ $symbol $branch ](fg:#AFAFFF bg:#2A2680)]($style)";
-    };
+          git_branch = {
+            symbol = "";
+            style = "bg:#2A2680";
+            format = "[[ $symbol $branch ](fg:#AFAFFF bg:#2A2680)]($style)";
+          };
 
-    git_status = {
-      style = "bg:#2A2680";
-      format = "[[($all_status$ahead_behind )](fg:#AFAFFF bg:#2A2680)]($style)";
-    };
+          git_status = {
+            style = "bg:#2A2680";
+            format = "[[($all_status$ahead_behind )](fg:#AFAFFF bg:#2A2680)]($style)";
+          };
 
-    nodejs = {
-      symbol = "";
-      style = "bg:#1C1970";
-      format = "[[ $symbol ($version) ](fg:#9FA8FF bg:#1C1970)]($style)";
-    };
+          nodejs = {
+            symbol = "";
+            style = "bg:#1C1970";
+            format = "[[ $symbol ($version) ](fg:#9FA8FF bg:#1C1970)]($style)";
+          };
 
-    bun = {
-      symbol = "";
-      style = "bg:#1C1970";
-      format = "[[ $symbol ($version) ](fg:#9FA8FF bg:#1C1970)]($style)";
-    };
+          bun = {
+            symbol = "";
+            style = "bg:#1C1970";
+            format = "[[ $symbol ($version) ](fg:#9FA8FF bg:#1C1970)]($style)";
+          };
 
-    rust = {
-      symbol = "";
-      style = "bg:#1C1970";
-      format = "[[ $symbol ($version) ](fg:#9FA8FF bg:#1C1970)]($style)";
-    };
+          rust = {
+            symbol = "";
+            style = "bg:#1C1970";
+            format = "[[ $symbol ($version) ](fg:#9FA8FF bg:#1C1970)]($style)";
+          };
 
-    golang = {
-      symbol = "";
-      style = "bg:#1C1970";
-      format = "[[ $symbol ($version) ](fg:#9FA8FF bg:#1C1970)]($style)";
-    };
+          golang = {
+            symbol = "";
+            style = "bg:#1C1970";
+            format = "[[ $symbol ($version) ](fg:#9FA8FF bg:#1C1970)]($style)";
+          };
 
-    php = {
-      symbol = "";
-      style = "bg:#1C1970";
-      format = "[[ $symbol ($version) ](fg:#9FA8FF bg:#1C1970)]($style)";
-    };
+          php = {
+            symbol = "";
+            style = "bg:#1C1970";
+            format = "[[ $symbol ($version) ](fg:#9FA8FF bg:#1C1970)]($style)";
+          };
 
-    time = {
-      disabled = false;
-      time_format = "%R";
-      style = "bg:#020152";
-      format = "[[  $time ](fg:#C2C6FF bg:#020152)]($style)";
-    };
+          time = {
+            disabled = false;
+            time_format = "%R";
+            style = "bg:#020152";
+            format = "[[  $time ](fg:#C2C6FF bg:#020152)]($style)";
+          };
 
-    character = {
-      success_symbol = "[❯](#6905F7)";
-      error_symbol = "[❯](#FF5C57)";
-      vimcmd_symbol = "[❮](#6905F7)";
-    };
+          character = {
+            success_symbol = "[❯](#6905F7)";
+            error_symbol = "[❯](#FF5C57)";
+            vimcmd_symbol = "[❮](#6905F7)";
+          };
 
-    cmd_duration = {
-      format = "[$duration]($style) ";
-      style = "yellow";
-    };
+          cmd_duration = {
+            format = "[$duration]($style) ";
+            style = "yellow";
+          };
 
-    nix_shell = {
-      symbol = "❄️ ";
-      format = "[$symbol]($style)";
-    };
+          nix_shell = {
+            symbol = "❄️ ";
+            format = "[$symbol]($style)";
+          };
 
-    shell = {
-      disabled = false;
-      style = "cyan";
-      bash_indicator = "";
-      powershell_indicator = "";
-    };
-  };
-};
+          shell = {
+            disabled = false;
+            style = "cyan";
+            bash_indicator = "";
+            powershell_indicator = "";
+          };
+        };
+      };
+    })
+  ];
+}
