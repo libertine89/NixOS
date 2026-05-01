50ae2f3f1969f5993e7bd2b4fa7795982adb1946
Chris <cdraper_electrical@hotmail.co.uk>
2026-05-01 19:57:44 +0100

    almost done

--- modules/core/starship.nix
+++ modules/core/starship.nix
@@ -5,14 +5,14 @@
       programs.starship = {
         enable = true;
         settings = {
-          format = "[░▒▓](#0A0373)[ 󱄅 ](bg:#0A0373 fg:#E6E6FF)[](fg:#0A0373 bg:#1C1690)$directory[](fg:#1C1690 bg:#3A36B5)$fill[](fg:#2A2680 bg:#2A2680)$git_branch$git_status[](fg:#1C1970 bg:#1C1970)$nodejs$bun$rust$golang$php[](fg:#020152 bg:#020152)$time[▓▒░](fg:#020152)\n$character";
+          format = "[░▒▓](#0A0373)[ 󱄅 ](bg:#0A0373 fg:#E6E6FF)[](fg:#0A0373 bg:#1C1690)[](fg:#1C1690 bg:#3A36B5)$directory[](fg:#3A36B5)$fill[](fg:#2A2680 bg:#2A2680)$git_branch$git_status[](fg:#1C1970 bg:#1C1970)$nodejs$bun$rust$golang$php[](fg:#020152 bg:#020152)$time[▓▒░](fg:#020152)\n$character";
 
           fill = {
             symbol = " ";
           };
 
           directory = {
-            style = "fg:#E6E6FF bg:#1C1690";
+            style = "fg:#E6E6FF bg:#3A36B5";
             format = "[ $path ]($style)";
             truncation_length = 3;
             truncation_symbol = "…/";
@@ -70,11 +70,11 @@
             disabled = false;
             time_format = "%R";
             style = "bg:#020152";
-            format = "[[  $time ](fg:#C2C6FF bg:#020152)]($style)";
+            format = "[[  $time ](fg:#E6E6FF bg:#020152)]($style)";
           };
 
           character = {
-            success_symbol = "[❯](#6905F7)";
+            success_symbol = "[❯](#E6E6FF)";
             error_symbol = "[❯](#FF5C57)";
             vimcmd_symbol = "[❮](#6905F7)";
           };
