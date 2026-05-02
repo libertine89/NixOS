ec3ff4f75595978dfd02eef80198cf648cac99b6
libertine89 <38113173+libertine89@users.noreply.github.com>
2026-05-02 08:50:07 +0100

    Fix variable escaping in toggle-kitty-quake script

--- modules/desktop/hyprland/scripts/toggle-kitty-quake.nix
+++ modules/desktop/hyprland/scripts/toggle-kitty-quake.nix
@@ -3,13 +3,12 @@
 pkgs.writeShellScriptBin "toggle-kitty-quake" ''
   #!/usr/bin/env bash
 
-  WIN_ID=$(hyprctl clients -j | ${pkgs.jq}/bin/jq -r '.[] | select(.initialClass=="kitty-quake") | .address' | head -n1)
+  WIN_ID=\$(hyprctl clients -j | ${pkgs.jq}/bin/jq -r '.[] | select(.initialClass=="kitty-quake") | .address' | head -n1)
 
-  if [ -z "$WIN_ID" ]; then
+  if [ -z "\$WIN_ID" ]; then
     hyprctl dispatch exec "kitty --class kitty-quake &"
     exit 0
   else
-    hyprctl dispatch focuswindow address:$WIN_ID
-    hyprctl dispatch killactive
+    hyprctl dispatch closewindow address:\$WIN_ID
   fi
 ''
