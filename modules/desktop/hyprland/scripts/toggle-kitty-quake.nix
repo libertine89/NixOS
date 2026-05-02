60a000bf0201639cc4743d6d4c01e417d7435b49
libertine89 <38113173+libertine89@users.noreply.github.com>
2026-05-02 08:47:17 +0100

    Fix toggle-kitty-quake script for window management

--- modules/desktop/hyprland/scripts/toggle-kitty-quake.nix
+++ modules/desktop/hyprland/scripts/toggle-kitty-quake.nix
@@ -1,16 +1,15 @@
 { pkgs }:
 
 pkgs.writeShellScriptBin "toggle-kitty-quake" ''
-
   #!/usr/bin/env bash
 
-  WIN_ID=$(hyprctl clients -j | ${pkgs.jq}/bin/jq -r '.[] | select(.initialClass=="'"kitty-quake"'") | .address' | head -n1)
+  WIN_ID=$(hyprctl clients -j | ${pkgs.jq}/bin/jq -r '.[] | select(.initialClass=="kitty-quake") | .address' | head -n1)
 
   if [ -z "$WIN_ID" ]; then
     hyprctl dispatch exec "kitty --class kitty-quake &"
     exit 0
   else
-    hyprctl dispatch focuswindow --class kitty-quake
-    hyprctl dispatch killactive  
-    fi
+    hyprctl dispatch focuswindow address:$WIN_ID
+    hyprctl dispatch killactive
+  fi
 ''
