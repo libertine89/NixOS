9df9fef76321d531de2e0c0503bcc7edd8f7c841
Chris <cdraper_electrical@hotmail.co.uk>
2026-05-02 08:12:26 +0100

    added exec once kitty-quake script

---
+++ modules/desktop/hyprland/scripts/exec-once-kitty-quake.nix
@@ -0,0 +1,15 @@
+{ pkgs }:
+
+pkgs.writeShellScriptBin "toggle-kitty-quake" ''
+  #!${pkgs.bash}/bin/env bash
+
+  CLASS="kitty-quake"
+
+  WIN_ID=$(hyprctl clients -j | ${pkgs.jq}/bin/jq -r \
+    '.[] | select(.initialClass=="'"$CLASS"'") | .address' | head -n1)
+
+  if [ -z "$WIN_ID" ]; then
+    hyprctl dispatch exec "kitty --class kitty-quake &
+    exit 0
+  fi
+''
