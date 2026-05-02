8f8a3f415afe8febb4218532da6f303ba3accd47
libertine89 <38113173+libertine89@users.noreply.github.com>
2026-05-02 12:36:54 +0100

    Remove Oh My Zsh and update aliases
    Removed Oh My Zsh configuration and added new aliases.

--- modules/core/zsh.nix
+++ modules/core/zsh.nix
@@ -10,11 +10,6 @@
       {
         programs.zsh = {
           enable = true;
-          oh-my-zsh = {
-            enable = true;
-            theme = "robbyrussell";
-            plugins = ["git" "sudo" ];
-          };
           autosuggestion.enable = false; # Loaded lazily via zsh-defer
           syntaxHighlighting.enable = false; # Loaded lazily via zsh-defer
           enableCompletion = false; # Loaded lazily via zsh-defer
@@ -147,6 +142,7 @@
                   echo "$N" |  figlet -c | lolcat &&  sleep 1
               done
             '';
+            c = 'clear';
             cls = "clear";
             tml = "tmux list-sessions";
             tma = "tmux attach";
@@ -158,17 +154,25 @@
             tree = "${pkgs.eza}/bin/eza --icons=auto --tree"; # dir tree
             vc = "code --disable-gpu"; # gui code editor
             nv = "nvim";
+            v = '$EDITOR';
+            vim = '$EDITOR';
             nf = "${pkgs.microfetch}/bin/microfetch";
             ff = "fastfetch";
+            nf = 'fastfetch';
+            pf = 'fastfetch';
             cp = "cp -iv";
             mv = "mv -iv";
             rm = "rm -vI";
             bc = "bc -ql";
             mkd = "mkdir -pv";
+            rmdir = "rm -ri";
+            df = "df -H";
             tp = "${pkgs.trash-cli}/bin/trash-put";
             tpr = "${pkgs.trash-cli}/bin/trash-restore";
             grep = "grep --color=always";
-            pokemon = "pokego --random 1-8 --no-title";
+            egrep = "egrep --color=auto";
+            fgrep = "fgrep --color=auto";
+            shutdown = 'systemctl poweroff';
 
             # Nixos
             list-gens = "nixos-rebuild list-generations";
@@ -184,6 +188,16 @@
             projects = "cd /mnt/work/Projects/";
             proj = "cd /mnt/work/Projects/";
             dev = "cd /mnt/work/Projects/";
+            root = "cd /";
+            home = "cd $HOME";
+           . = "cd ..";
+           .. = "cd ..";
+           ... = "cd ../..";
+           ..2 = "cd ../..";
+           ..3 = "cd ../../..";
+           ..4 = "cd ../../../..";
+           ..5 = "cd ../../../../..";
+
           };
         };
       }
