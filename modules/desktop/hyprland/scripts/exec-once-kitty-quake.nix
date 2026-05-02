2d2a204601c0f6b52f21939797891118b24a5d7a
libertine89 <38113173+libertine89@users.noreply.github.com>
2026-05-02 10:02:54 +0100

    Update exec-once-kitty-quake script logic
    Modify the script to focus on an existing window or execute a new instance of kitty-quake.

--- modules/desktop/hyprland/scripts/exec-once-kitty-quake.nix
+++ modules/desktop/hyprland/scripts/exec-once-kitty-quake.nix
@@ -1,15 +1,14 @@
-{ pkgs }:
-
 pkgs.writeShellScriptBin "exec-once-kitty-quake" ''
   #!${pkgs.bash}/bin/env bash
 
   CLASS="kitty-quake"
 
   WIN_ID=$(hyprctl clients -j | ${pkgs.jq}/bin/jq -r \
-    '.[] | select(.initialClass=="'"$CLASS"'") | .address' | head -n1)
+    '.[] | select(.class=="'"$CLASS"'" or .initialClass=="'"$CLASS"'") | .address' | head -n1)
 
   if [ -z "$WIN_ID" ]; then
-    hyprctl dispatch exec "kitty --class kitty-quake" &
-    exit 0
+    hyprctl dispatch exec "kitty --class kitty-quake"
+  else
+    hyprctl dispatch focuswindow address:$WIN_ID
   fi
 ''
