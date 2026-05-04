cf8ca7334789cb276041524545edf7d8630d353f
libertine89 <38113173+libertine89@users.noreply.github.com>
2026-05-04 19:37:41 +0100

    Refactor keybindings configuration in keybindings.nix

--- modules/desktop/niri/confs/keybindings.nix
+++ modules/desktop/niri/confs/keybindings.nix
@@ -10,10 +10,9 @@ binds {
     Mod+T hotkey-overlay-title="Open a Terminal: kitty" { spawn "kitty"; }   
     Mod+D hotkey-overlay-title="Run an Application: fuzzel" { spawn "fuzzel"; }
 
-    Super+Alt+L { hotkey-overlay-title="Lock the Screen: swaylock"; spawn "swaylock"; }
+    Super+Alt+L hotkey-overlay-title="Lock the Screen: swaylock" { spawn "swaylock"; }
 
-    Super+Alt+S allow-when-locked=true {
-        hotkey-overlay-title=null;
+    Super+Alt+S allow-when-locked=true hotkey-overlay-title=null {
         spawn-sh "pkill orca || exec orca";
     }
 
