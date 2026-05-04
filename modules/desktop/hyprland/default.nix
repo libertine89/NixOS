4bab61ccb4f386ba74e676ea34bc31c0636adfbb
Chris <cdraper_electrical@hotmail.co.uk>
2026-05-04 09:52:18 +0100

    creating desktop based script folders inside scripts folder

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -22,24 +22,24 @@ let
     ;
 
   # scripts (unchanged)
-  autoclicker = pkgs.callPackage ../scripts/autoclicker.nix { };
-  batterynotify = pkgs.callPackage ../scripts/batterynotify.nix { };
-  clipmanager = pkgs.callPackage ../scripts/clipmanager.nix { };
-  fileManagerScript = pkgs.callPackage ../scripts/file-manager.nix { inherit terminal; };
-  gamemode = pkgs.callPackage ../scripts/gamemode.nix { };
-  keyboardswitch = pkgs.callPackage ../scripts/keyboardswitch.nix { };
-  keybinds-yad = pkgs.callPackage ../scripts/keybinds-yad.nix { };
-  rofimusic = pkgs.callPackage ../scripts/rofimusic.nix { };
-  screen-record = pkgs.callPackage ../scripts/screen-record.nix { };
-  screenshot = pkgs.callPackage ../scripts/screenshot.nix { };
-  wallpaper = pkgs.callPackage ../scripts/wallpaper.nix { inherit defaultWallpaper; };
-  zoom = pkgs.callPackage ../scripts/zoom.nix { };
-  wrap-workspaces-next = pkgs.callPackage ../scripts/wrap-workspaces-next.nix {};
-  wrap-workspaces-prev = pkgs.callPackage ../scripts/wrap-workspaces-prev.nix {};
-  wrap-window-workspace-next = pkgs.callPackage ../scripts/wrap-window-workspace-next.nix {};
-  wrap-window-workspace-prev = pkgs.callPackage ../scripts/wrap-window-workspace-prev.nix {};
-  toggle-layout = pkgs.callPackage ../scripts/toggle-layout.nix {};
-  exec-once-kitty-quake = pkgs.callPackage ../scripts/exec-once-kitty-quake.nix {};
+  autoclicker = pkgs.callPackage ../scripts/hyprscripts/autoclicker.nix { };
+  batterynotify = pkgs.callPackage ../scripts/hyprscripts/batterynotify.nix { };
+  clipmanager = pkgs.callPackage ../scripts/hyprscripts/clipmanager.nix { };
+  fileManagerScript = pkgs.callPackage ../scripts/hyprscripts/file-manager.nix { inherit terminal; };
+  gamemode = pkgs.callPackage ../scripts/hyprscripts/hyprscripts/gamemode.nix { };
+  keyboardswitch = pkgs.callPackage ../scripts/hyprscripts/keyboardswitch.nix { };
+  keybinds-yad = pkgs.callPackage ../scripts/hyprscripts/keybinds-yad.nix { };
+  rofimusic = pkgs.callPackage ../scripts/hyprscripts/rofimusic.nix { };
+  screen-record = pkgs.callPackage ../scripts/hyprscripts/screen-record.nix { };
+  screenshot = pkgs.callPackage ../scripts/hyprscripts/screenshot.nix { };
+  wallpaper = pkgs.callPackage ../scripts/hyprscripts/wallpaper.nix { inherit defaultWallpaper; };
+  zoom = pkgs.callPackage ../scripts/hyprscripts/zoom.nix { };
+  wrap-workspaces-next = pkgs.callPackage ../scripts/hyprscripts/wrap-workspaces-next.nix {};
+  wrap-workspaces-prev = pkgs.callPackage ../scripts/hyprscripts/wrap-workspaces-prev.nix {};
+  wrap-window-workspace-next = pkgs.callPackage ../scripts/hyprscripts/wrap-window-workspace-next.nix {};
+  wrap-window-workspace-prev = pkgs.callPackage ../scripts/hyprscripts/wrap-window-workspace-prev.nix {};
+  toggle-layout = pkgs.callPackage ../scripts/hyprscripts/toggle-layout.nix {};
+  exec-once-kitty-quake = pkgs.callPackage ../scripts/hyprscripts/exec-once-kitty-quake.nix {};
 
   ctx = {
     inherit pkgs lib getExe getExe'
