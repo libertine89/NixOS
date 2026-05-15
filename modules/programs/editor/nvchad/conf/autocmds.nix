d409ddf71aa0977be4fe5f628cf31ad8319e5f15
Chris <cdraper_electrical@hotmail.co.uk>
2026-05-15 15:57:23 +0100

    added more keybinds and autocmds to nvchad lua

--- modules/programs/editor/nvchad/conf/autocmds.nix
+++ modules/programs/editor/nvchad/conf/autocmds.nix
@@ -28,4 +28,25 @@ require "nvchad.autocmds"
       vim.lsp.buf.format({ async = false })
     end,
   })
+
+-- Auto-Remove trailing spaces
+  vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*",
+      callback = function()
+          vim.cmd([[%s/\s\+$//e]])
+      end,
+  })
+
+-- Remember Cursor Position  
+  vim.api.nvim_create_autocmd("BufReadPost", {
+    pattern = "*",
+      callback = function()
+          local mark = vim.api.nvim_buf_get_mark(0, '"')
+          local line = mark[1]
+          if line > 1 and line <= vim.api.nvim_buf_line_count(0) then
+              vim.api.nvim_win_set_cursor(0, mark)
+          end
+      end,
+  })
 ''
+
--- modules/programs/editor/nvchad/conf/mappings.nix
+++ modules/programs/editor/nvchad/conf/mappings.nix
@@ -7,12 +7,36 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+  
+  -- Cycle Modes
+    map({ "n", "i", "v" }, "<C-@>", function()
+        local mode = vim.fn.mode()
+        if mode == "n" then
+            -- Normal → Insert
+            vim.api.nvim_feedkeys("i", "n", true)
+        elseif mode == "i" then
+            -- Insert → Visual
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<ESC>v", true, false, true), "n", true)
+        elseif mode == "v" then
+            -- Visual → Normal
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<ESC>", true, false, true), "n", true)
+        end
+    end, { desc = "Cycle modes: Normal → Insert → Visual → Normal" })
+    
+  -- Close current buffer
+    map({ "n", "i", "v" }, "<C-q>", function()
+        if vim.fn.mode() ~= "n" then
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<ESC>", true, false, true), "n", true)
+        end
+        vim.cmd("bp | bd #")
+    end, { desc = "Close current buffer (Ctrl-Q)" })
 
-  -- normal mode line navigation
+  -- Normal mode line navigation
     map("n", "<C-b>", "0", { desc = "Go to start of line" })
     map("n", "<C-e>", "$", { desc = "Go to end of line" })
 
-  -- insert mode paste
+  -- Insert mode paste
     map("i", "<C-p>", "<C-r>+", { desc = "Paste from clipboard" })
 
 ''
+
