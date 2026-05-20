1656f12d54dc9bdcf333efd9890bc66bf2e0a37b
Chris <cdraper_electrical@hotmail.co.uk>
2026-05-20 21:02:51 +0100

    checking last things

--- modules/core/oh-my-posh.nix
+++ modules/core/oh-my-posh.nix
@@ -6,7 +6,6 @@
   ];
 
   programs.zsh.interactiveShellInit = ''
-    eval "$(oh-my-posh init zsh --config ~/.config/oh-my-posh/default.omp.json)"
   '';
 
   programs.bash.interactiveShellInit = ''
