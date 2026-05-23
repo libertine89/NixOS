3d286228c607ea2bdaaaafba9469719e3a0528c3
Chris <cdraper_electrical@hotmail.co.uk>
2026-05-23 11:12:26 +0100

    refactor complete, before testing rebuild

--- modules/core/home-manager.nix
+++ modules/core/home-manager.nix
@@ -1,17 +1,9 @@
 {
-  pkgs,
   inputs,
   host,
-  lib,
-  config,
   ...
 }:
-let
-  inherit (import ../../hosts/${host}/variables.nix)
-    username
-    shell
-    ;
-in
+
 {
   imports = [ inputs.home-manager.nixosModules.home-manager ];
   programs.dconf.enable = true; # Enable dconf for home-manager
