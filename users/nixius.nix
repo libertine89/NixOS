d6086e2d7b79d109c3084e35c494fb26ffff8adb
libertine89 <38113173+libertine89@users.noreply.github.com>
2026-05-23 11:05:59 +0100

    Refactor user configuration to use variables

--- users/nixius.nix
+++ users/nixius.nix
@@ -1,36 +1,31 @@
-{ host, config, lib, pkgs, ... }:
+{ host, config, pkgs, lib ... }:
 
 let
-  inherit (import ../hosts/${host}/variables.nix)
-    username browser terminal editor shellPrompt
-  ;
+  vars = import ../hosts/${host}/variables.nix;
 in
 {
-    users.${username} = {
-      # Let Home Manager install and manage itself.
-      programs.home-manager.enable = true;
-      xdg.enable = true;
+  home.username = vars.username;
+  home.homeDirectory = "/home/${vars.username}";
+  home.stateVersion = "26.05";
 
-      home = {
-        username = "${username}";
-        homeDirectory = "/home/${username}";
-        stateVersion = "26.05"; # Do not change!
-        sessionVariables = {
-          EDITOR =
-            if (editor == "nixvim" || editor == "neovim" || editor == "nvchad") then
-              "nvim"
-            else if editor == "vscode" then
-              "code"
-            else
-              "nano";
-          BROWSER = "${browser}";
-          TERMINAL = "${terminal}";
+  xdg.enable = true;
 
-          # if (shellPrompt == "starship") =
-          home.file.".config/starship.toml".source =
-            config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/NixOS/modules/core/starship/starship.toml";
-        };
-      };
-    };
+  sessionVariables = {
+    EDITOR =
+      if (vars.editor == "nixvim" || vars.editor == "neovim" || vars.editor == "nvchad") then
+        "nvim"
+      else if vars.editor == "vscode" then
+        "code"
+      else
+        "nano";
 
+    BROWSER = vars.browser;
+    TERMINAL = vars.terminal;
+  };
+  
+  home.file = lib.optionalAttrs (vars.shellPrompt == "starship") {
+    ".config/starship.toml".source =
+      config.lib.file.mkOutOfStoreSymlink
+        "${config.home.homeDirectory}/NixOS/modules/core/starship/starship.toml";
+  };
 }
