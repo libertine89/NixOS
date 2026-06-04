06af67b8b49f2c3710cbde77bfc977ad13844558
libertine89 <38113173+libertine89@users.noreply.github.com>
2026-06-04 18:25:32 +0100

    Update variables.nix

--- hosts/Default/variables.nix
+++ hosts/Default/variables.nix
@@ -29,7 +29,7 @@
   videoDriver = "amdgpu"; # nvidia, amdgpu, intel
   nvidiaChannel = "legacy_580"; # stable, latest, beta, legacy_xxx
   bluetoothSupport = false; # Whether your motherboard supports bluetooth
-
+### HELLO
   # Localization
   timezone = "Europe/London";
   locale = "en_GB.UTF-8";
