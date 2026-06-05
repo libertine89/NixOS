961af3e1c9edd720298e367945b2c6f39c1f27c7
libertine89 <38113173+libertine89@users.noreply.github.com>
2026-06-05 07:55:53 +0100

    Clean up comments in variables.nix
    Removed unnecessary 'GOODBYE' comments from variables.nix.

--- hosts/Default/variables.nix
+++ hosts/Default/variables.nix
@@ -3,7 +3,7 @@
 
   # Desktop Environment
   desktop = "niri"; # hyprland, niri, i3, gnome, plasma6
-
+### GOODBYE
   # Theme & Appearance
   bar = "noctalia-shell"; # waybar, hyprpanel, noctalia-shell, caelestia-shell, dms-shell
   waybarTheme = "minimal"; # stylish, minimal
@@ -13,7 +13,7 @@
                             # flame, glowing, green_blocks, hexagon_dots_alt, hud_2, motion, square
   defaultWallpaper = "abstract101.png"; # Change with SUPER + SHIFT + W (Hyprland)
   hyprlockWallpaper = "abstract101.png";
-
+### GOODBYE
   # Default Applications
   terminal = "kitty"; # kitty, alacritty
   editor = "nvchad"; # nixvim, vscode, helix, doom-emacs, nvchad, neovim
@@ -23,7 +23,7 @@
   shell = "zsh"; # zsh, bash
   shellPrompt = "starship"; # oh-my-posh, starship
   games = false ; # Enable/Disable gaming module
-
+### GOODBYE
   # Hardware
   hostname = "Nixius";
   videoDriver = "amdgpu"; # nvidia, amdgpu, intel
