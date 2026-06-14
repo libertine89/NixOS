4e4a35009b7bb784e01dbf535fd9ad4ab156c2cc
chris <cdraperelectrical@gmail.com>
2026-06-14 15:10:40 +0100

    nearly finished with set up of zed, might just need to implment custom theme so its declarative or back by git

--- modules/user/nixius/zed/zed-settings.nix
+++ modules/user/nixius/zed/zed-settings.nix
@@ -1,4 +1,54 @@
 {
+  # General & Themes
+  vim_mode = true;
+  vim = {
+    cursor_shape = {
+      normal = "block";
+      insert = "bar";
+      visual = "hollow";
+      replace = "underline";
+    };
+  };
+  relative_line_numbers = "wrapped";
+  theme = "Catppuccin Mocha";
+  icon_theme = "Catppuccin Mocha";
+  cursor_blink = true;
+  hover_popover_enabled = true;
+  hover_popover_delay = 300;
+  autosave = "on_focus_change";
+  format_on_save = "on";
+  current_line_highlight = "all";
+  active_pane_modifiers = {
+    inactive_opacity = 0.8;
+  };
+  which_key = {
+    enabled = true;
+  };
+  session = {
+    trust_all_worktrees = true;
+  };
+  toolbar = {
+    agent_review = false;
+    selections_menu = false;
+    quick_actions = false;
+    breadcrumbs = false;
+  };
+  # Brackets & Indentation Coloring
+  colorize_brackets = true;
+  indent_guides = {
+    enabled = true;
+    coloring = "indent_aware";
+    background_colouring = "indent_aware";
+  };
+  # Font  & Sizes
+  buffer_font_family = "JetBrainsMono Nerd Font";
+  ui_font_size = 16;
+  buffer_font_size = 12.0;
+  agent_buffer_font_size = 10.0;
+  agent_ui_font_size = 12.0;
+  # Scrolling
+  horizontal_scroll_margin = 60.0;
+  vertical_scroll_margin = 10.0;
   scrollbar = {
     show = "never";
   };
@@ -10,10 +60,11 @@
     show = "always";
   };
 
-  current_line_highlight = "all";
-
-  vim = {
-    toggle_relative_line_numbers = true;
+  agent = {
+    sidebar_side = "right";
+    dock = "right";
+    favorite_models = [ ];
+    model_parameters = [ ];
   };
 
   agent_servers = {
@@ -22,8 +73,8 @@
     };
   };
 
-  active_pane_modifiers = {
-    inactive_opacity = 0.8;
+  project_panel = {
+    dock = "left";
   };
 
   git_panel = {
@@ -31,39 +82,61 @@
     tree_view = false;
   };
 
-  agent = {
-    sidebar_side = "right";
-    dock = "right";
-    favorite_models = [];
-    model_parameters = [];
+  tabs = {
+    git_status = true;
   };
 
-  project_panel = {
-    dock = "left";
+  title_bar = {
+    show_branch_status_icon = true;
   };
 
-  vim_mode = true;
+  languages = {
+    TypeScript = {
+      formatter = {
+        external = {
+          command = "prettier";
+          args = [
+            "--stdin-filepath"
+            "{buffer_path}"
+          ];
+        };
+      };
+    };
 
-  icon_theme = "Zed (Default)";
-  ui_font_size = 16;
-  buffer_font_size = 12.0;
-  theme = "Ayu Dark";
+    TSX = {
+      formatter = {
+        external = {
+          command = "prettier";
+          args = [
+            "--stdin-filepath"
+            "{buffer_path}"
+          ];
+        };
+      };
+    };
 
-  horizontal_scroll_margin = 60.0;
-  vertical_scroll_margin = 10.0;
+    JavaScript = {
+      formatter = {
+        external = {
+          command = "prettier";
+          args = [
+            "--stdin-filepath"
+            "{buffer_path}"
+          ];
+        };
+      };
+    };
 
-  which_key = {
-    enabled = true;
-  };
-
-  autosave = "on_focus_change";
-
-  agent_buffer_font_size = 10.0;
-  agent_ui_font_size = 12.0;
-
-  buffer_font_family = "JetBrainsMono Nerd Font";
-
-  session = {
-    trust_all_worktrees = true;
+    JSON = {
+      formatter = {
+        external = {
+          command = "prettier";
+          args = [
+            "--stdin-filepath"
+            "{buffer_path}"
+          ];
+        };
+      };
+    };
   };
 }
