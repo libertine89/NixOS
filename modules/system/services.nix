c593bc7c295c239852fc11e5068c939fbb6c4cba
chris <cdraperelectrical@gmail.com>
2026-06-18 10:42:11 +0100

    kanata setup

--- modules/system/services.nix
+++ modules/system/services.nix
@@ -15,6 +15,43 @@
     #   scheduler = "scx_lavd"; # https://github.com/sched-ext/scx/blob/main/scheds/rust/README.md
     # };
 
+    kanata = {
+      enable = true;
+      keyboards = {
+        internalKeyboard = {
+          devices = [
+            "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
+            "/dev/input/by-id/usb-Framework_Laptop_16_Keyboard_Module_-_ANSI_FRAKDKEN0100000000-event-kbd"
+            "/dev/input/by-id/usb-Framework_Laptop_16_Keyboard_Module_-_ANSI_FRAKDKEN0100000000-if02-event-kbd"
+          ];
+          extraDefCfg = "process-unmapped-keys yes";
+          config = ''
+            (defsrc
+            a s d f j k l ;
+            )
+            (defvar
+            tap-time 150
+            hold-time 200
+            )
+            (defalias
+            a (tap-hold $tap-time $hold-time a lmet)
+            s (tap-hold $tap-time $hold-time s lalt)
+            d (tap-hold $tap-time $hold-time d lsft)
+            f (tap-hold $tap-time $hold-time f lctl)
+            j (tap-hold $tap-time $hold-time j rctl)
+            k (tap-hold $tap-time $hold-time k rsft)
+            l (tap-hold $tap-time $hold-time l ralt)
+            ; (tap-hold $tap-time $hold-time ; rmet)
+            )
+
+            (deflayer base
+            @a  @s  @d  @f  @j  @k  @l  @;
+            )
+          '';
+        };
+      };
+    };
+
     openssh = {
       enable = true;
       ports = [ 22 ];
